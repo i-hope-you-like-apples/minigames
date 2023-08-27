195abea26cb2b6b7f44469300afb81f309608666
i-hope-you-like-apples <i-hope-you-like-apples@users.noreply.github.com>
2023-08-27 22:19:05 +0200

    Fix absolute links

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [svelte()],
-  base: "minigames/",
+  base: "minigames/dist/",
   build: {
     rollupOptions: {
       input: {
