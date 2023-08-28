0bec58f7459f4b17fb97b17ac1695b15d1e88318
i-hope-you-like-apples <i-hope-you-like-apples@users.noreply.github.com>
2023-08-28 21:43:58 +0200

    Add a PeerJS proof of concept

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,43 @@
 <script lang="ts">
-  import Counter from "./Counter.svelte";
+  import Peer from "peerjs";
+
+  let peerId = "";
+
+  function handleHost() {
+    let peer = new Peer();
+    peer.on("open", function (id) {
+      console.log("My peer ID is: " + id);
+    });
+    peer.on("connection", function (conn) {
+      conn.on("data", function (data) {
+        // Will print 'hi!'
+        console.log(data);
+        conn.send("Hello back!");
+      });
+    });
+  }
+
+  function handleJoin() {
+    let peer = new Peer();
+    console.log(peerId);
+    let conn = peer.connect(peerId);
+    conn.on("open", function () {
+      conn.on("data", function (data) {
+        console.log("Received", data);
+      });
+      conn.send("hi!");
+    });
+  }
 </script>
 
 <main class="container">
-  <h1>Hello, I hope you like apples</h1>
+  <h1 style="margin-top: 55px;">Hello, I hope you like apples</h1>
 
-  <p>Here are some minigames!</p>
+  <p>Here are some minigames for you!</p>
 
-  <div class="card">
-    <Counter />
-  </div>
+  <button on:click={handleHost}>Click to host a game</button>
+
+  <input bind:value={peerId} type="text" placeholder="Enter a game peer ID" />
+
+  <button on:click={handleJoin}>Click to join a game</button>
 </main>
